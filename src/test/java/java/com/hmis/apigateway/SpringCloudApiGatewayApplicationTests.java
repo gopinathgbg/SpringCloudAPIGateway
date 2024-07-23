fd6edcb94c539bdb14c2ecd7e92e89f7986467dc
gopinathgbg <gopinath.vbg@gmail.com>
2024-07-24 00:12:32 +0530

    SpringCloud API Gateway Created

---
+++ src/test/java/java/com/hmis/apigateway/SpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.hmis.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
